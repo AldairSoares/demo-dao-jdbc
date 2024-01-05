a86b292e3861eef8d5812ba6d4473a223cd414b4
Aldair Soares <aldairsoares@Aldairs-MacBook-Air.local>
2024-01-05 18:34:12 -0300

    [REFACTOR]: Reusing Instantiation - Department and Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,11 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller sl = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println("=== TEST 1: seller findById");
 		
-		System.out.print(sl);
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.print(seller);
 	}
 
 }
