5818e7146c106b3f57e5b2829df5e5326e32d10a
Aldair Soares <aldairsoares@Aldairs-MacBook-Air.local>
2024-01-05 20:36:49 -0300

    [FEAT]: Delete Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -46,13 +49,21 @@ public class Program {
 		
 		System.out.println("Inserted new seller! Id = " + newSeller.getId());
 		
-		System.out.println("=== TEST 5: seller insert ===");
+		System.out.println("=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update done!");
+		
+		System.out.println("=== TEST 6: seller delete ===");
+		
+		System.out.println("Insert the seller id to be deleted");
+		int n = sc.nextInt();
+		
+		sellerDao.deleteById(n);
+		System.out.println("Deletion completed");
 
-
+		sc.close();
 	}
 
 }
